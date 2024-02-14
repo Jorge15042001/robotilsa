e73978d6ef8870e3c0c4f885d6368ce07d4a6f46
Jorge10542001 <adrivuglarin@hotmail.com>
2024-02-14 01:14:34 -0500

    handling responses correctly

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #  from util import *
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 
 from flasgger import Swagger
 from flasgger import swag_from
@@ -13,25 +13,30 @@ app = Flask(__name__)
 swagger = Swagger(app)
 
 
+def response_setup(response):
+    if response["success"]:
+        return make_response(response, 200)
+    return make_response(response, 400)
+
+
 @swag_from("./documentation/sensing_interval.yaml")
 @app.route("/system/processor", methods=['POST'])
 def update_sensing_interval():
-    return {
-        "success": True,
-        #  "str_err": str(e)
-    }
+    return response_setup({
+        "success": False,
+        "str_err": "Not implemented"
+    })
 
 
 @swag_from("./documentation/get_date.yaml")
 @app.route("/system/date", methods=['GET'])
 def get_system_date():
     timestamp = datetime.datetime.now().timestamp()
-    print(timestamp)
-    return {
+    return response_setup({
         "success": True,
         "current_timestamp": int(timestamp),
         "str_err": ""
-    }
+    })
 
 
 @swag_from("./documentation/set_date.yaml")
@@ -40,36 +45,35 @@ def set_system_date():
     payload = request.get_json()
     target_timestamp = payload["current_timestamp"]
     if type(target_timestamp) is not int:
-        return {
+        return response_setup({
             "success": False,
             "str_err": "Timestamp en formato incorrecto"
-        }
+        })
     sucess, str_err = set_system_time(target_timestamp)
 
-    return {
+    return response_setup({
         "success": sucess,
         "str_err": str_err
-    }
+    })
 
 
 @swag_from("./documentation/get_hidrophones.yaml")
 @app.route("/controller/hydrophone/all", methods=['GET'])
 def get_hydrophones():
-    return {
-        "success": True,
-        "value": "",
-        "str_err": ""
-    }
+    return response_setup({
+        "success": False,
+        "str_err": "Not implemented"
+    })
 
 
 @swag_from("./documentation/get_hidrophone.yaml")
 @app.route("/controller/hydrophone/<id>", methods=['GET'])
 def get_hydrophone(id):
-    return {
-        "success": True,
-        "value": "",
-        "str_err": ""
-    }
+    return response_setup({
+        "success": False,
+        "str_err": "Not implemented"
+    })
+
 
 #  @swag_from("./documentation/sensors.getall.yaml")
 
